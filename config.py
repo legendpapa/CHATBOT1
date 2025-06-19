3510d65044a205f9dcb7e233c64b36476b14dcb6
legendpapa <tosif4004@gmail.com>
2025-06-19 16:15:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "8113054986:AAGhPZs8lTovJQ2Cc-RlnwTY8UMC5UdOBmc")
+BOT_TOKEN = getenv("BOT_TOKEN", "7420275716:AAHUxjzAerHvQEVIzzRY_7by_Iu84U_CShY")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/")
-OWNER_ID = int(getenv("OWNER_ID", "7588172591"))
-SUPPORT_GRP = getenv("SUPPORT_GRP", "PBX_CHAT")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "HEROKUBIN_01")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "II_BAD_BABY_II")
+OWNER_ID = int(getenv("OWNER_ID", "7650291301"))
+SUPPORT_GRP = getenv("SUPPORT_GRP", "ZOYU_SUPPORT")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "THE_INCRICIBLE")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "LEGEND_MICKEY")
 API = getenv("API", "http://3.0.146.239:8000/chatbot?api_key=chatai.Badmunda.workers&message=")
